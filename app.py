6eec8e23ad5dbd76993ff8d6c12f04b9decd24e2
honolulu0 <909919553@qq.com>
2021-01-07 18:18:18 +0800

    Initial commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config.from_pyfile('config.py')
 app.config.from_object(debugConfig)
 #app 的配置
 db.init_app(app)
-# 测试推送 666666333
+# 测试推送 666666
 # 初始化数据库对象
 if __name__ == '__main__':
     app.run('127.0.0.1',8888)
